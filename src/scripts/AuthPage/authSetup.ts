77db58883dddd19063722abc92a0c6cc0d7719ac
Shubham Chamola <shubhamchamola1@gmail.com>
2022-10-12 10:55:57 +0530

    Setup Authentication
    - setup auth for the app
    - signup with google
    - signup with email
    - signup with anonymous
    - add styling for the auth page
    - created method to check user input
    - created necessary layout for displaying errors while signing up

---
+++ src/scripts/AuthPage/authSetup.ts
@@ -0,0 +1,64 @@
+import { authObject } from "./auth";
+import store from "../Store/store";
+
+// ***
+// On click of JOIN NOW btn
+authObject.alternateModeButton.addEventListener("click", () => {
+  if (authObject.alternateModeButton.textContent == "JOIN NOW") {
+    authObject.signUpConstruct();
+  } else {
+    authObject.signInConstruct();
+  }
+});
+// ---------------------------------------------
+
+// ***
+// Form Input handler
+authObject.emailInput.addEventListener("change", () => {
+  authObject.checkEmailInput();
+});
+authObject.passwordInput.addEventListener("change", () => {
+  authObject.checkPasswordInput();
+});
+// ---------------------------------------------
+
+// ***
+// form Button Hanlder
+authObject.formButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (authObject.formButton.innerText == "LOGIN") {
+    authObject.signInHandler();
+  } else {
+    authObject.signUpHandler();
+  }
+});
+// ----------------------------------------------
+
+// ***
+// Guest singin event
+authObject.guestButton.addEventListener("click", () => {
+  authObject.guestSignIn();
+});
+// ----------------------------------------------
+
+// ***
+// Google singin event
+authObject.googleButton.addEventListener("click", () => {
+  authObject.googleSignIn();
+});
+// ----------------------------------------------
+
+// ***
+// Event for displaying authPage
+const authDomManipulation = function () {
+  let { loggedIn } = store.getState().authState;
+  if (loggedIn) {
+    authObject.removeAuthFromDOM();
+  } else {
+    authObject.addAuthInDOM();
+  }
+};
+
+store.subscribe(authDomManipulation);
+// -----------------------------------------------
+export default "";
